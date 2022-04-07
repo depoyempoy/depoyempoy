c62daeb2ada8d45571240eab690281b58356411b
depoyempoy <30864931+depoyempoy@users.noreply.github.com>
2022-04-08 00:18:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-node app.js
+// Importing http core package/module provided by nodejs
+
+var http = require('http');
+
+// Creating a server
+
+http.createServer(function (req, res) {
+
+// Write response as Html(text)
+
+res.writeHead(200, {'Content-Type': 'text/html'});
+
+// Writing static text
+
+res.end('Hello World');
+
+// Server listening on port number 4300
+
+}).listen(4300);
